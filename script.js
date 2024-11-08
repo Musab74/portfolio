c5589fe4d46cb31f4db03a994b92f1d6856e482b
Musab74 <rmcbaxt@gmail.com>
2024-11-09 03:52:14 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const menu=document.querySelector("menu");
-const nav=document.querySelector(".links");
+const menu = document.querySelector("#menu"); // Use ID here
+const nav = document.querySelector(".links");
 
-menu.onclick=()=>{
-    menu.classList.toggle('bx-x');
-    nav.classList.toggle('active');
-}
+menu.onclick = () => {
+    menu.classList.toggle('bx-x'); // Change icon to an "x" on click
+    nav.classList.toggle('active'); // Toggle the visibility of the links
+};
